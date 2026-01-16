cfc264df406bfc83aa96873820ff51080e6ce33d
enzoperalva <pessoalenzopinheiro@gmail.com>
2026-01-16 11:43:25 -0300

    Novos códigos, mais organizados. Partiu World 2!

--- World 1- Python/teste 21-raiz quadrada.py
+++ World 1- Python/teste 21-raiz quadrada.py
@@ -1,7 +1,11 @@
-'''import math
+import math
+
+#1° FORMA:
 num= float(input('Digíte um númrero:'))
 raiz= math.sqrt(num)
-print (f'A raiz de {num} é {raiz:.1f}')'''
+print (f'A raiz de {num} é {raiz:.1f}')
+
+#2° FORMA:
 
 n1= float(input('Escolha um número:'))
 n2= n1 ** (1/2)
