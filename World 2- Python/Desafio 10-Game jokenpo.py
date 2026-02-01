d3277e68c2c42df8236b807bb273ed922e724736
Enzoperalva <pessoalenzopinheiro@gmail.com>
2026-02-01 20:34:12 -0300

    Finish

--- World 2- Python/Desafio 10-Game jokenpo.py
+++ World 2- Python/Desafio 10-Game jokenpo.py
@@ -19,6 +19,11 @@ time.sleep(0.7)
 
 #VARIÁVEIS:
 opcao= str(input('Opção:')).lower()
+if opcao not in lista:
+    print('\033[31mJOGADA INVALIDA\033[m')
+    print('Escolha pedra, papel ou tesoura!')
+    exit()
+
 print('JO')
 time.sleep(0.4)
 print('KEN')
@@ -29,47 +34,14 @@ time.sleep(0.5)
 print('--='*10)
 #CONDIÇÕES DE DEORRTA E EMPATE:
         #PEDRA:
-if opcao == 'pedra' and 'papel' and 'tesoura':
-
-    if bot == 'pedra' and opcao== 'tesoura':
-        print(f'\033[31mPERDEDOR!!!!\033[m')
-        print(f'Eu escolhi {bot}')
-
-    elif bot == 'pedra' and opcao== 'pedra':
-        print('\033[36mEMPATAMOS\033[m')
-        print('Ambos de nós escolhemos pedra')
-
-    elif bot == 'pedra' and opcao== 'papel':
-        print(f'\033[32mGANHADOR\033[m')
-        print(f'Eu escolhi {bot}')
-
-        #PAPEL:
-    elif bot== 'papel' and opcao== 'tesoura':
-        print(f'\033[32mGANHADOR\033[m')
-        print(f'Eu escolhi {bot}')
-
-    elif bot== 'papel' and opcao== 'pedra':
-        print(f'\033[31mPERDEDOR!!!!\033[m')
-        print(f'Eu escolhi {bot}')
-
-    elif bot== 'papel' and opcao== 'papel':
-        print('\033[36mEMPATAMOS\033[m')
-        print('Ambos de nós escolhemos papel')
-
-        #TESOURA:
-    elif bot== 'tesoura' and opcao== 'tesoura':
-        print('\033[36mEMPATAMOS\033[m')
-        print('Ambos de nós escolhemos tesoura')
-
-    elif bot== 'tesoura' and opcao== 'papel':
-        print(f'\033[31mPERDEDOR!!!!\033[m')
-        print(f'Eu escolhi {bot}')
-
-    elif bot== 'tesoura' and opcao== 'pedra':
-        print(f'\033[32mGANHADOR\033[m')
-        print(f'Eu escolhi {bot}')
+if opcao == bot:
+    print('\033[36mEMPATAMOS\033[m')
+    print(f'Ambos de nós escolhemos {bot}')
+elif(opcao == 'pedra' and bot == 'tesoura') or \
+    (opcao == 'tesoura' and bot == 'papel') or \
+    (opcao == 'papel' and bot == 'pedra'):
+    print(f'\033[32mGANHADOR\033[m')
+    print(f'Eu escolhi {bot}')
 else:
-    print('\033[31mJOGADA INVALIDA\033[m')
-    print('Escolha pedra, papel ou tesoura!')
-
+    print('\033[31mVOCÊ PERDEU!\033[m')
 print('--='*10)
