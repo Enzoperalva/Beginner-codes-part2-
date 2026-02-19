2336371cb8073471a1d59e6ce9b96f3af9077632
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-02-19 08:13:12 -0300

    Estudos de modulos

---
+++ modulos/Teste1.py
@@ -0,0 +1,6 @@
+from uteis import numeros
+num = int(input('Digíte o valor: '))
+fat = numeros.fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O duplo de {num} é {numeros.dobro(num)}')
+print(f'O triplo de {num} é {numeros.triplo(num)}')
---
+++ modulos/moeda.py
@@ -0,0 +1,16 @@
+def aumentar(n,por):
+    tot= n * (por/100)
+    return n+tot
+
+
+def diminuir(n, por):
+    tot= n* (por / 100)
+    return n- tot
+
+
+def dobro(n):
+    return n * 2
+
+
+def metdade(n):
+    return n / 2
